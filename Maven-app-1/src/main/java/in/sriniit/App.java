dfd91f52d8e0d5a681befe2bb25bd431a7076895
Raju <mnraju2517@gmail.com>
2023-09-29 20:07:18 +0530

    1002 changes

--- Maven-app-1/src/main/java/in/sriniit/App.java
+++ Maven-app-1/src/main/java/in/sriniit/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+       //Changes related to 1002 task
+
+        System.out.println("Hello AWS");
     }
 }
