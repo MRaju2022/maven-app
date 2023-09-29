a060abf9a2de9e5621fe4fe2d198fb988ad41e12
Raju <mnraju2517@gmail.com>
2023-09-29 20:11:52 +0530

    1001 changes

--- Maven-app-1/src/main/java/in/sriniit/App.java
+++ Maven-app-1/src/main/java/in/sriniit/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+       //Chnages related to 1001 task
+
+        System.out.println("Hello DevOps");
+
         System.out.println( "Hello World!" );
 
        //Changes related to 1002 task
